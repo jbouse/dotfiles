e058d812a1cb4fe5c64ed1bc037516da4d22b02d
Jeremy T. Bouse <jbouse@users.noreply.github.com>
2025-02-27 16:39:56 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-if [ ! -d /go/bin ]; then
-  mkdir -p /go/bin
-fi
+HUGO_LATEST=$(curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest | jq -r '. as $artifacts | .tag_name | ltrimstr("v") as $version | $artifacts | .assets | .[] | [.name, .browser_download_url] |  if (.[0] | contains($version) and contains("extended") and (contains("withdeploy") | not) and contains("Linux-64bit") and contains(".tar.gz")) then .[1]  else empty end')
 
-go install -tags extended github.com/gohugoio/hugo@latest
+curl -sL $HUGO_LATEST | sudo tar -C /usr/local/hugo/bin -xzf - hugo
