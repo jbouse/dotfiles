23e9e1e29c14a05e9a025bb695971ef08cbbc526
Jeremy T. Bouse <jbouse@users.noreply.github.com>
2025-06-06 20:32:28 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,5 +5,5 @@ HUGO_LATEST=$(curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest
 curl -sL $HUGO_LATEST | sudo tar -C /usr/local/hugo/bin -xzf - hugo
 
 wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install terraform
